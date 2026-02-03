3481b5b0fd1e3c1d903e1d520e5fda8df1319911
Pomiyilma <pomiyilma@gmail.com>
2026-02-03 18:00:24 +0300

    Add function to find longest common prefix in strings (Leetcode)

---
+++ Longest-common-prefix.py
@@ -0,0 +1,16 @@
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        if not strs:
+            return ""
+
+        result = ""
+        for i in range(len(strs[0])):
+            current_char = strs[0][i]
+            for word in strs:
+                if i >= len(word) or word[i] != current_char:
+                    return result
+            result += current_char
+
+        return result
+
+        
