454b23d0f8c2dd2f17e66b0fd5914165ec9f89be
Pomiyilma <pomiyilma@gmail.com>
2026-02-04 10:02:02 +0300

    Add All the integers in a range are covered (Leetcode)
    Add isCovered method to check if all integers in the specified range are covered by given ranges.

---
+++ Integers-covered.py
@@ -0,0 +1,12 @@
+class Solution:
+    def isCovered(self, ranges: List[List[int]], left: int, right: int) -> bool:
+        for num in range (left, right + 1):
+            is_covered = False
+
+            for start, end in ranges:
+                if start <= num <= end:
+                    is_covered = True
+                    break
+            if not is_covered:
+                return False
+        return True
