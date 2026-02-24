feeaaef670f273879e78336ecb4d6427f31c1edf
Pomiyilma <pomiyilma@gmail.com>
2026-02-24 14:28:44 +0300

    Add Minimum Number of Arrows to Burst Balloons using intervals method (Leetcode)
    Implement a solution to find the minimum number of arrows needed to burst balloons based on their positions.

---
+++ Minimum-number-of-arrows-to-burst-balloons.py
@@ -0,0 +1,14 @@
+class Solution:
+    def findMinArrowShots(self, points: List[List[int]]) -> int:        #exONnb
+        points.sort()   #o(nlogn) time
+        res = len(points)
+        prev = points[0]
+
+        for i in range (1, len(points)):
+            curr = points[i]
+            if prev[1] >= curr[0]:
+                res -= 1
+                prev = [curr[0], min(curr[1], prev[1])]
+            else:
+                prev = curr
+        return res
