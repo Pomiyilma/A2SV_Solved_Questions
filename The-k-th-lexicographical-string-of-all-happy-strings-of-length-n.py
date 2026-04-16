5e4bd0e97212b7404997cf58017fb22d19442029
Pomiyilma <pomiyilma@gmail.com>
2026-04-16 18:18:54 +0300

    Add The k-th Lexicographical String of All Happy Strings of Length n using Backtracking and Pruning (Leetcode)

---
+++ The-k-th-lexicographical-string-of-all-happy-strings-of-length-n.py
@@ -0,0 +1,26 @@
+class Solution:
+    def getHappyString(self, n: int, k: int) -> str:
+        result = []
+        path = []
+
+        def backtrack():
+            if len(result) >= k:
+                return
+
+            if len(path) == n:
+                result.append("".join(path))
+                return
+
+            for ch in ['a', 'b', 'c']:
+                if path and path[-1] == ch:
+                    continue
+
+                path.append(ch)
+                backtrack()
+                path.pop()
+
+        backtrack()
+
+        if len(result) < k:
+            return ""
+        return result[k - 1]
