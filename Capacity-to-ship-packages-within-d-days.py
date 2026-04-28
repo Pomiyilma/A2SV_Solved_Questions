bb44e3f8655106e57d6142486fc9bd752e0866e4
Pomiyilma <pomiyilma@gmail.com>
2026-04-28 18:10:18 +0300

    Add Capacity To Ship Packages Within D Days (Leecode)

---
+++ Capacity-to-ship-packages-within-d-days.py
@@ -0,0 +1,21 @@
+class Solution:
+    def shipWithinDays(self, weights: List[int], days: int) -> int:
+    
+        def canShip(capacity):
+            D, curr = 1,0
+            for w in weights:
+                if curr + w > capacity:
+                    D += 1
+                    curr = 0
+                curr += w
+            return D <= days
+
+        left, right = max(weights),sum(weights)
+
+        while left < right:
+            mid = (left + right) // 2
+            if canShip(mid):
+                right = mid 
+            else:
+                left = mid + 1
+        return left
